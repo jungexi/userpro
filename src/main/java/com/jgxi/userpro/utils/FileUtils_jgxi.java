83664bd8e96f405d0afe55bf95e79f38caab95cd
jungexi <41530350+jungexi@users.noreply.github.com>
2020-08-13 15:19:01 +0800

    Update FileUtils_jgxi.java
    修改了秘钥

--- src/main/java/com/jgxi/userpro/utils/FileUtils_jgxi.java
+++ src/main/java/com/jgxi/userpro/utils/FileUtils_jgxi.java
@@ -12,8 +12,8 @@ public class FileUtils_jgxi {
 // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "http://oss-cn-qingdao.aliyuncs.com";
 // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        String accessKeyId = "LTAI4Fghmm6JcmJ8ExsAzPPk";
-        String accessKeySecret = "ayBWeKkNCingqYfLo1KBAfZEUWh70W";
+        String accessKeyId = "LTAI4Fghmm6Jc8ExsPPk";
+        String accessKeySecret = "ayBWeKkNngqYo1KBAfZEUWh70W";
 
 // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
